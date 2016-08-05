5ba39951b4072458ee172ecb2ae49de3b3d47a27
Henry Roe <hroe@hroe.me>
2016-08-04 21:25:57 -0700

    cleaned up cruft

--- example_pull_metadata.py
+++ example_pull_metadata.py
@@ -10,18 +10,10 @@ rawtxt = subprocess.Popen("mdimport -d2 " + filename, shell=True,
 
 MDtag = 'DCTPMD'
 
-re.findall(MDtag + '-([^-])*-START(.*?)' + MDtag + '-\1-END', rawtxt)
-
-re.findall('(' + MDtag + '-([^-]*)-START)', rawtxt)
-
-re.findall('(' + MDtag + '-([^-]*)-START)(.*?)' + MDtag, rawtxt)
-
-re.findall('(' + MDtag + '-([^-]*)-START)(.*?)' + MDtag + r"-\2", rawtxt)
-
-re.findall(MDtag + '-([^-]*)-START(.*?)' + MDtag + r"-\1", rawtxt)
-
 def process_tags(input):
     return {a[0]:process_tags(a[1].strip()) if a[1].strip().startswith(MDtag + '-') else a[1].strip() 
             for a in re.findall(MDtag + '-([^-]*)-START(.*?)' + MDtag + r"-\1", input)}
 
-print process_tags(rawtxt)
+metadata = process_tags(rawtxt)
+
+print(metadata)
